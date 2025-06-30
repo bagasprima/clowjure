19f206330804c90cb9854f31d8d58c83a1a8dbe1
bagasprima <bagasprima@live.com>
2025-06-30 16:24:56 +0700

    fix(project.clj): add log for sl4j in dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,21 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.stuartsierra/component "1.1.0"]
+                 
+                 [cheshire "6.0.0"]
+                 [compojure "1.7.0"]
                  [io.pedestal/pedestal.service "0.5.10"]
                  [io.pedestal/pedestal.jetty "0.5.10"]
-                 [compojure "1.7.0"]
                  [ring/ring-core "1.11.0"]
-                 [ring/ring-jetty-adapter "1.9.6"]      
-                 [ring/ring-defaults "0.3.4"]
-                 [cheshire "6.0.0"]
+                 [ring/ring-jetty-adapter "1.9.6"]
+                 [ring/ring-defaults "0.3.4"] ; simple frontend
                  [enlive "1.1.6"]
-                 [com.stuartsierra/component "1.1.0"]]
+
+                 ;; loggings
+                 ;;  [ch.qos.logback/logback-classic "1.4.11"]
+                 [org.slf4j/slf4j-api "1.7.36"]
+                 [org.slf4j/slf4j-simple "1.7.36"]]
   :main ^:skip-aot clow.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
